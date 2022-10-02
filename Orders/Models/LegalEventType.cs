ff75096c5544986372c7a38c2ac2a11d3114486c
Giuliano <PJuliano@verisk.com>
2022-10-02 09:25:56 -0400

    Master Reference

--- Orders/Models/LegalEventType.cs
+++ Orders/Models/LegalEventType.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Types;
 using Legislative.Schema;
@@ -15,13 +16,20 @@ namespace Legislative.Models
             AnalysisService = analysisService;
             Field(o => o.Id);
             Field(o => o.Name);
-            Field(o => o.Description);
-
-
-            FieldAsync<LmonAnalysisType>("analysis", resolve: Resolve);
             Field(o => o.Created);
             Field(o => o.Status);
+
+            FieldAsync<LmonAnalysisType>("analysis", resolve: Resolve);
+            Field(o => o.Description);
+
+            // tbd            Field<ListGraphType<NonNullGraphType<StringGraphType>>>("imagesList", resolve: x => x.Source?.ImagesList)
+            Field<ListGraphType<MasterReferenceType>>("jurisdictions", resolve: x => x.Source?.jurisdiction.ToList().AsReadOnly());
+
+
         }
+
+        
+
         async Task<object> Resolve(IResolveFieldContext<LegalEvent> context)
         {
             var sourceCustomerId = context.Source.CustomerId;
---
+++ Orders/Models/MasterReferenceType.cs
@@ -0,0 +1,14 @@
+﻿using GraphQL.Types;
+using Legislative.Repository;
+
+namespace Legislative.Models
+{
+    public class MasterReferenceType : ObjectGraphType<MasterReference>
+    {
+        public MasterReferenceType()
+        {
+            Field(m => m.refCode);
+            Field(m => m.refDescription);
+        }
+    }
+}
