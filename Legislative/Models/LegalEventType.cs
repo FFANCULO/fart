4dcf21213bdf2460dc62de58f6d3124578ed2908
Giuliano <PJuliano@verisk.com>
2022-10-03 10:41:54 -0400

    Master Reference Field resolution

--- Legislative/Models/LegalEventType.cs
+++ Legislative/Models/LegalEventType.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Types;
-using Legislative.Schema;
 using Legislative.Services;
 
 namespace Legislative.Models;
--- Legislative/Models/LmonAnalysisType.cs
+++ Legislative/Models/LmonAnalysisType.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using GraphQL.Types;
-using Legislative.Models;
+using Legislative.Repository;
 
-namespace Legislative.Schema
+namespace Legislative.Models
 {
     public class LmonAnalysisType : ObjectGraphType<LmonAnalysis>
     {
-        
+
         public LmonAnalysisType()
         {
             Field(c => c.Id);
@@ -19,18 +21,18 @@ namespace Legislative.Schema
             Field(c => c.LegalEventId);
             Field(c => c.LegalEventRevision);
             Field(c => c.RecordType);
-        //    Field(c => c.ReportDate);
+            //    Field(c => c.ReportDate);
             Field(c => c.CircularLink);
             Field(c => c.Comment);
             Field(c => c.PersonId);
 
             // tbd  -- product_type
             Field<ListGraphType<MasterReferenceType>>("products",
-                resolve: x => x.Source?.product_type.ToList().AsReadOnly());
+                resolve: x => x.Source?.product_type.ToList().AsReadOnly() ?? new ReadOnlyCollection<MasterReference>(new List<MasterReference>()));
             Field<ListGraphType<MasterReferenceType>>("lobs",
-                resolve: x => x.Source?.line_of_business.ToList().AsReadOnly());
+                resolve: x => x.Source?.line_of_business.ToList().AsReadOnly() ?? new ReadOnlyCollection<MasterReference>(new List<MasterReference>()));
             Field<ListGraphType<MasterReferenceType>>("requirements",
-                resolve: x => x.Source?.proc_requirement.ToList().AsReadOnly());
+                resolve: x => x.Source?.proc_requirement.ToList().AsReadOnly() ?? new ReadOnlyCollection<MasterReference>(new List<MasterReference>()));
         }
     }
 }
