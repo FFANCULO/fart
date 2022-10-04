d319040f9950c880372864ad0e115e285b4e9d50
Giuliano <PJuliano@verisk.com>
2022-10-04 12:48:19 -0400

    cleanup

--- Legislative/Models/LmonAnalysis.cs
+++ Legislative/Models/LmonAnalysis.cs
@@ -5,17 +5,7 @@ using Legislative.Repository;
 
 namespace Legislative.Models
 {
-    //public class LmonAnalysis
-//{
-//    public LmonAnalysis(int id, string name)
-//    {
-//        Id = id;
-//        Name = name;
-//    }
 
-//    public int Id { get; private set; }
-//    public string Name { get; set; }
-//}
 
     public class LmonAnalysis
     {
